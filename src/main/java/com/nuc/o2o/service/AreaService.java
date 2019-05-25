5090d77204737ed598659f25de04618fe8e08b21
chen <m15903462872@163.com>
2019-05-25 20:20:35 +0800

    update code

--- src/main/java/com/nuc/o2o/service/AreaService.java
+++ src/main/java/com/nuc/o2o/service/AreaService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 /**
  * @author Calvin
- * @Description:
+ * @Description:获得区域
  */
 public interface AreaService {
 
--- src/main/java/com/nuc/o2o/web/superadmin/AreaController.java
+++ src/main/java/com/nuc/o2o/web/superadmin/AreaController.java
@@ -2,6 +2,8 @@ package com.nuc.o2o.web.superadmin;
 
 import com.nuc.o2o.entity.Area;
 import com.nuc.o2o.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,12 +22,15 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
 
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listArea() {
+        logger.info("======start======");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMap = new HashMap<>();
         List<Area> list = new ArrayList<>();
         try {
@@ -37,6 +42,10 @@ public class AreaController {
             modelMap.put("success", false);
             modelMap.put("errMsg", e.toString());
         }
+        logger.error("test error");
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]", endTime - startTime);
+        logger.info("======end======");
         return modelMap;
     }
 }
